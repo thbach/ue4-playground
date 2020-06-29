bb20ab78209d6e715e17de906c7c1b36dc8629e5
Thai Bach <thbach@gmail.com>
2020-06-29 11:55:07 -0700

    start pacman

---
+++ pacman/Source/pacman/pacmanGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "pacmanGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PACMAN_API ApacmanGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
